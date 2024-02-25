1abc36f6b6f1f8b9223a6e6b18562ef06c75d912
metablog <khu10735@naver.com>
2024-02-26 02:37:05 +0900

    feat: axios image-upload post

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
-
+import axios from "axios";
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("이미지를 업로드 해주세요.");
+
+  const imageSelectHandler = (e) => {
+    const imageFile = e.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log({ res });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="image">{fileName}</label>
-      <input
-        type="file"
-        id="image"
-        onChange={(e) => {
-          const imageFile = e.target.files[0];
-          setFile(imageFile);
-          setFileName(imageFile.name);
-        }}
-      />
+      <input type="file" id="image" onChange={imageSelectHandler} />
       <button type="submit">제출</button>
     </form>
   );
